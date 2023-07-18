09c22a95a791583a8a5f6b5fe55d3db57523b1fd
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-18 00:17:39 -0300

    Adicioanndo todos os tokens do nosso tema

---
+++ src/theme/index.ts
@@ -0,0 +1,54 @@
+export const THEME = {
+  colors: {
+    product: {
+      yellow_dark: '#C47F17',
+      yellow: '#DBAC2C',
+      purple_dark: '#4B2995',
+      purple: '#8047F8',
+      purple_light: '#EBE5F9',
+    },
+    base: {
+      gray_100: '#272221',
+      gray_200: '#403937',
+      gray_300: '#574F4D',
+      gray_400: '#8D8686',
+      gray_500: '#D7D5D5',
+      gray_600: '#E6E5E5',
+      gray_700: '#EDEDED',
+      gray_800: '#F3F2F2',
+      gray_900: '#FAFAFA',
+      white: '#FFFFFF'
+    },
+    feedback: {
+      red_dark: '#C44117',
+      red: '#E8BAAB',
+      red_light: '#F2DFD8'
+    }
+  },
+  font_family: {
+    baloo: {
+      bold: 'Baloo2_700Bold'
+    },
+    roboto: {
+      regular: 'Roboto_400Regular',
+      bold: 'Roboto_700Bold'
+    }
+  },
+  font_size: {
+    baloo: {
+      title_xl: 36,
+      title_lg: 24,
+      title_md: 20,
+      title_sm: 16,
+      title_xs: 14,
+    },
+    roboto: {
+      text_lg: 20,
+      text_md: 16,
+      text_sm: 14,
+      text_xs: 12,
+      tag: 10,
+      button: 14
+    }
+  }
+}
