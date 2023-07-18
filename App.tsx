517d213a9facd6e9773ec59c3ab367f5d06a764d
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-18 00:16:32 -0300

    Transformando o app em TS e adicionando fontes do google

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useFonts as useFontsRoboto, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useFonts as useFontsBaloo, Baloo2_700Bold } from '@expo-google-fonts/baloo-2';
+import { THEME } from './src/theme';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontRobotoLoaded] = useFontsRoboto({ Roboto_400Regular, Roboto_700Bold })
+  const [fontBalooLoaded] = useFontsBaloo({ Baloo2_700Bold })
+
+  if (!fontRobotoLoaded || !fontBalooLoaded) {
+    return (
+      <ActivityIndicator />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style='light' backgroundColor={THEME.colors.base.gray_100} />
+      <Home />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
