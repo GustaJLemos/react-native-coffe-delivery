9c0bd52bacf4bf10cddb6408bc6fc64ef7323ca8
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-18 22:18:43 -0300

    Continuando o desenvolvimento da interface

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, ScrollView } from 'react-native';
 import { MagnifyingGlass } from 'phosphor-react-native';
 import { styles } from './styles';
 import { Header } from '../../components/Header';
 import { THEME } from '../../theme';
+import { CoffePrincipalCard } from '../../components/CoffePrincipalCard';
+import { Filter } from '../../components/Filter';
+import { CoffeCard } from '../../components/CoffeCard';
+
+const fazermockdps = [1, 2, 3]
 
 export function Home() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.background} />
 
       <Header />
@@ -24,6 +29,39 @@ export function Home() {
           placeholderTextColor={THEME.colors.base.gray_400}
         />
       </View>
-    </View>
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.principalCoffes}
+        contentContainerStyle={styles.principalCoffesContent}
+      >
+        {fazermockdps.map((item) => (
+          <CoffePrincipalCard key={item} />
+        ))}
+      </ScrollView>
+
+      <Text style={styles.filterTitle}>
+        Nossos cafés
+      </Text>
+
+      <View style={styles.filterContainer}>
+        {fazermockdps.map((item) => (
+          <Filter key={item} />
+        ))}
+      </View>
+
+      <Text style={styles.coffeListTitle}>
+        tradicionais
+      </Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12 }}
+      >
+        {fazermockdps.map((item) => (
+          <CoffeCard key={item} />
+        ))}
+      </ScrollView>
+    </ScrollView>
   );
 }
