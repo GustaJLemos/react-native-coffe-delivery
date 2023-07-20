a59f0fc8c9ba039a9307980cde3b2bb10d901b08
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-20 17:58:24 -0300

    Adicionando o disabled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,18 +7,21 @@ import { THEME } from '../../theme';
 type Props = TouchableOpacityProps & {
   title: string;
   type: 'purple' | 'yellow';
+  disabled?: boolean;
 }
 
-export function Button({ title, type, ...rest }: Props) {
+export function Button({ title, type, disabled, ...rest }: Props) {
   // TODO tenq fazer a animação de onFocus aq
   return (
     <TouchableOpacity
       style={[
         styles.container,
         {
-          backgroundColor: type === 'purple' ? THEME.colors.product.purple_dark : THEME.colors.product.yellow_dark
+          backgroundColor: type === 'purple' ? THEME.colors.product.purple_dark : THEME.colors.product.yellow_dark,
+          opacity: disabled ? 0.5 : 1
         }
       ]}
+      disabled={disabled}
       {...rest}
     >
       <Text style={styles.text}>
