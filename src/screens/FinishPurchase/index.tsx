e5009d7aa8ade8ce9d29191c0745498b291950fc
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-20 16:42:32 -0300

    Terminadno de desenvolver a interface

--- src/screens/FinishPurchase/index.tsx
+++ src/screens/FinishPurchase/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { View, Text } from 'react-native';
+import IlustrationSvg from '../../assets/coffes/Illustration.svg'
 import { styles } from './styles';
+import { Button } from '../../components/Button';
 
 export function FinishPurchase() {
   return (
     <View style={styles.container}>
+      <IlustrationSvg />
 
+      <View>
+        <Text style={styles.title}>
+          Uhu! Pedido confirmado
+        </Text>
+        <Text style={styles.text}>
+          Agora é só aguardar que logo o café {'\n'} chegará até você!
+        </Text>
+
+        <Button
+          title='Ir para a home'
+          type='purple'
+        />
+      </View>
     </View>
   );
 }
--- src/screens/FinishPurchase/styles.ts
+++ src/screens/FinishPurchase/styles.ts
@@ -7,5 +7,19 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: THEME.colors.base.gray_900,
+  },
+  title: {
+    color: THEME.colors.product.yellow_dark,
+    fontFamily: THEME.font_family.baloo.bold,
+    fontSize: THEME.font_size.baloo.title_lg,
+    alignSelf: 'center',
+    marginTop: 24
+  },
+  text: {
+    color: THEME.colors.base.gray_200,
+    fontFamily: THEME.font_family.roboto.regular,
+    fontSize: THEME.font_size.roboto.text_sm,
+    textAlign: 'center',
+    marginBottom: 36
   }
 });
