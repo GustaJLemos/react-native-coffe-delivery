2d3ce0857fda54b9716632eb6bfed926ccb6c2ad
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-21 11:07:31 -0300

    Criando utils para mostrar q quantidade total de itens no carrinho

--- src/utils/calculateAllQuantityItem.ts
+++ src/utils/calculateAllQuantityItem.ts
@@ -1,11 +1,11 @@
 import { CoffeAddedToCart } from "../types/CoffeAddedToCart";
 
-export function calculateItemPrices(coffesToCalculate: CoffeAddedToCart[]) {
+export function calculateAllQuantityItem(coffesToCalculate: CoffeAddedToCart[]) {
   let quantity: number = 0;
 
   coffesToCalculate.forEach((item) => {
     quantity = quantity + item.quantity
   })
 
-  return (quantity * 9.90).toFixed(2)
+  return quantity;
 }
