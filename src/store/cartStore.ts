9bf3a3493afd0eece0ddb488c8f65c1df6f63ca3
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-21 11:07:02 -0300

    Adicioanndo novas props

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -2,13 +2,22 @@ import { create } from 'zustand'
 import { CoffeAddedToCart } from '../types/CoffeAddedToCart';
 import { Coffes } from '../types/Coffes';
 
+type UpdateCoffeType = {
+  id: string,
+  newQuantity: number,
+  itemSize: string
+}
+
 type StoreProps = {
   coffeSelected: Coffes,
   setCoffeSelected: (coffe: Coffes) => void,
   coffeAddedToCart: CoffeAddedToCart[],
   setCoffeAddedToCart: (newCoffe: CoffeAddedToCart) => void,
   clearAllCoffeAddedToCart: () => void;
-  updateCoffeQuantityById: (id: string, newQuantity: number) => void;
+  updateCoffeQuantityById: ({ id, newQuantity, itemSize }: UpdateCoffeType) => void;
+  deleteCoffeAddedById: (id: string, size: string) => void;
+  showCoffeToast: boolean;
+  setShowCoffeToast: (showCoffe: boolean) => void;
 }
 
 export const useCartStore = create<StoreProps>((set) => ({
@@ -19,14 +28,25 @@ export const useCartStore = create<StoreProps>((set) => ({
     (currentState) => ({ coffeAddedToCart: [...currentState.coffeAddedToCart, newCoffe] })
   ),
   clearAllCoffeAddedToCart: () => set(() => ({ coffeAddedToCart: [] })),
-  updateCoffeQuantityById: (id: string, newQuantity: number) => set(
+  updateCoffeQuantityById: ({ id, newQuantity, itemSize }: UpdateCoffeType) => set(
     (currentState) => ({
       coffeAddedToCart: [
         ...currentState.coffeAddedToCart.map((item) => {
-          return item.id === id ? { ...item, quantity: newQuantity } : item
+          return item.id === id && item.size === itemSize ? { ...item, quantity: newQuantity } : item
         })
       ]
     })
-  )
+  ),
+  deleteCoffeAddedById: (id: string, size: string) => set(
+    (currentState) => ({
+      coffeAddedToCart: [
+        ...currentState.coffeAddedToCart.filter((item) => {
+          return !(item.id === id && item.size === size) && item
+        })
+      ]
+    })
+  ),
+  showCoffeToast: false,
+  setShowCoffeToast: (showCoffe: boolean) => set(() => ({ showCoffeToast: showCoffe })),
   // TODO fazer função de excluir um café em específico
 }))
