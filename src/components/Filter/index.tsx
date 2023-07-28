2397e50caa11a5fed05cc5da24c6d9b98e4ef702
GustaJLemos <gustavolemosmendes@gmail.com>
2023-07-27 22:53:24 -0300

    Criando transição para o valor selecionado

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,31 +1,53 @@
-import React from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, Pressable, PressableProps } from 'react-native';
 import { CoffeType } from '../../types/CoffeType';
 
 import { styles } from './styles';
 import { THEME } from '../../theme';
+import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
-type Props = TouchableOpacityProps & {
+const PressabledAnimated = Animated.createAnimatedComponent(Pressable);
+
+type Props = PressableProps & {
   filter: CoffeType,
   selected: boolean,
   onSelect: (filterSelected: CoffeType) => void;
 }
 
 export function Filter({ filter, selected, onSelect }: Props) {
+  const colorAnimation = useSharedValue(0);
+
+  const colorBgAnimatedStyles = useAnimatedStyle(() => {
+    return ({
+      backgroundColor: interpolateColor(colorAnimation.value, [0, 1], ['transparent', THEME.colors.product.purple]),
+    })
+  })
+
+  // fazer melhor essa animação aq, pra ela dar uma sumida mais suave
+  const colorTextAnimatedStyles = useAnimatedStyle(() => {
+    return ({
+      color: interpolateColor(colorAnimation.value, [0, 1], [THEME.colors.product.purple_dark, THEME.colors.base.white])
+    })
+  })
+
+  useEffect(() => {
+    colorAnimation.value = withTiming(selected ? 1 : 0, { duration: 400 });
+  }, [selected])
+
   return (
-    <TouchableOpacity
+    <PressabledAnimated
       onPress={() => onSelect(filter)}
       style={[
         styles.container,
-        { backgroundColor: selected ? THEME.colors.product.purple : 'transparent' }
+        colorBgAnimatedStyles
       ]}
     >
-      <Text style={[
+      <Animated.Text style={[
         styles.text,
-        { color: selected ? THEME.colors.base.white : THEME.colors.product.purple_dark }
+        colorTextAnimatedStyles
       ]}>
         {filter}
-      </Text>
-    </TouchableOpacity>
+      </Animated.Text>
+    </PressabledAnimated>
   );
 }
